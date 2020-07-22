450c2db0e6968c7cea9ee255e38324d5cad28013
Thao <thaomvo@gmail.com>
2020-07-22 01:59:20 -0700

    Updates

---
+++ precache-manifest.e3a2027a3c37bb08c3b77d2681939330.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed2369c75a788ef2748048667b6fb0e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c789bdc7b121b00fec9",
+    "url": "/static/css/main.c1ad3fde.chunk.css"
+  },
+  {
+    "revision": "aea9963a0b4945f270eb",
+    "url": "/static/js/2.2ad20f03.chunk.js"
+  },
+  {
+    "revision": "aa1beec61a0842def2074e46ff3c61b6",
+    "url": "/static/js/2.2ad20f03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c789bdc7b121b00fec9",
+    "url": "/static/js/main.0d20dded.chunk.js"
+  },
+  {
+    "revision": "2cde1a02d3115346b942",
+    "url": "/static/js/runtime-main.2e8ffcea.js"
+  },
+  {
+    "revision": "d6837aec184315cd59ec0b6f74460f38",
+    "url": "/static/media/arrow.d6837aec.svg"
+  },
+  {
+    "revision": "dfa4bb1430f3cf21aad2221703ad4530",
+    "url": "/static/media/automation.dfa4bb14.png"
+  },
+  {
+    "revision": "cd2528161ff31c0d41ff67c4c49d21e9",
+    "url": "/static/media/cny.cd252816.png"
+  },
+  {
+    "revision": "b122145d6de3f4dda2cb5b4d291c8cea",
+    "url": "/static/media/email.b122145d.svg"
+  },
+  {
+    "revision": "1b209d6530ec166fda236fc95e7c0f10",
+    "url": "/static/media/linkedin.1b209d65.svg"
+  },
+  {
+    "revision": "e6f15fc974083cca11e9b2fcee529062",
+    "url": "/static/media/logo.e6f15fc9.svg"
+  },
+  {
+    "revision": "86f37537df1ae1f6e1f1f78835170836",
+    "url": "/static/media/luxury.86f37537.png"
+  },
+  {
+    "revision": "13fc0e75f29ebb8b8fca105619b73105",
+    "url": "/static/media/main-kv.13fc0e75.svg"
+  },
+  {
+    "revision": "4de000ec16ea20e8ccda19ff7c23ff6b",
+    "url": "/static/media/trading.4de000ec.png"
+  },
+  {
+    "revision": "a9e8a715cd0eef04921d3f4ea9731290",
+    "url": "/static/media/travel.a9e8a715.png"
+  },
+  {
+    "revision": "ab5e67b3d5c9e3169dc91411a9e544da",
+    "url": "/static/media/what-im-up-to.ab5e67b3.svg"
+  }
+]);
